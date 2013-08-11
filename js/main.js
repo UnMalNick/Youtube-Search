3f80a9f7efdc2227e61c91034220f4c75d028733
Juan Pablo Rojas <jpablorp@warebeone.com>
2013-08-10 23:56:02 -0500

    delete console.log()

--- js/main.js
+++ js/main.js
@@ -22,7 +22,6 @@ function VideoTemplate(video){
 		category = video.category[1].label,
 		thumb = video.media$group.media$thumbnail[1].url,
 		embed = video.media$group.media$content[0].url.split('?')[0];
-	console.log(embed);
 
 	html = '<li class="item">';
 	html += '<figure class="image_item"> <img src="' + thumb + '"" /> </figure>';
