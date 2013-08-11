5fc089f68e97d53b901464302dcd8c56d8990570
Juan Pablo Rojas <jpablorp@warebeone.com>
2013-08-11 00:54:15 -0500

    change embed url

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function VideoTemplate(video){
 		author = video.author[0].name.$t,
 		category = video.category[1].label,
 		thumb = video.media$group.media$thumbnail[1].url,
-		embed = video.media$group.media$content[0].url.split('?')[0];
+		embed = video.media$group.media$content[0].url.split('?')[0].replace('/v/','/embed/');
 
 	html = '<li class="item">';
 	html += '<figure class="image_item"> <img src="' + thumb + '"" /> </figure>';
